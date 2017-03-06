bd32d699a95670f4ce075e61c81856e559fe18cb
Si Beaumont <simonjbeaumont@gmail.com>
2017-03-06 10:28:57 +0000

    mutt: C-q opens Quip links in app
    Signed-off-by: Si Beaumont <simonjbeaumont@gmail.com>

---
+++ mutt/open-quip-links.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+
+QUIP_BASE_URL = "https://quip.orchard.apple.com/"
+
+QUIP_SLUG_REGEX = re.compile(".*%s([^\s]*)" % QUIP_BASE_URL)
+
+EXCLUDE_SLUGS = [
+    "account/email",
+]
+
+for line in sys.stdin.readlines():
+    match = re.match(QUIP_SLUG_REGEX, line)
+    if match:
+        slug = match.group(1)
+        if slug in EXCLUDE_SLUGS:
+            continue
+        os.system("open quip://%s" % slug)
